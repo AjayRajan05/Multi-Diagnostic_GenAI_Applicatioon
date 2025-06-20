22b5a37e39738453fc58abb2b6471a34e976c3e2
AjayRajan05 <131186327+AjayRajan05@users.noreply.github.com>
2025-06-20 20:58:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,12 +178,11 @@ elif st.session_state["page"] == "Diabetes Manual":
          st.error("Sample Diabetes Report PDF not found. Please check file path.")
     except Exception as e:
          st.error(f"Error loading PDF: {e}")
-'''
-    with open("Sample Parameters/diabetes.pdf", "rb") as pdf_file:
-        base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
-        st.markdown(pdf_display, unsafe_allow_html=True)
-'''
+   # with open("Sample Parameters/diabetes.pdf", "rb") as pdf_file:
+   #     base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+    #    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
+    #    st.markdown(pdf_display, unsafe_allow_html=True)
+
 
 elif st.session_state["page"] == "Heart Disease Manual":
     if st.button("⬅️ Back to User Manual"):
